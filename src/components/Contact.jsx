917f7c42db99f9550034d8bdaafbf894789df62d
ibsa-a1 <ibsaabera712@gmail.com>
2025-09-02 11:56:01 +0300

    Update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -39,7 +39,7 @@ const Contact = () => {
     >
       <div className="max-w-7xl mx-auto">
         {" "}
-        <div className="text-center mb-16">
+        <div className="text-center mb-10">
           <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-gray-900 mb-6">
             Contact{" "}
             <span className="relative">
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,11 +7,19 @@ export default function Projects() {
       id="Projects"
       className="px-6 md:px-16 lg:px-24 py-12 mt-10 md:mt-16 sm:mt-8"
     >
-      <h1 className="text-2xl sm:text-4xl md:text-5xl font-bold md:mb-32 mb-16 mx-auto flex items-center justify-center">
+      {/* <h1 className="text-2xl sm:text-4xl md:text-5xl font-bold md:mb-32 mb-16 mx-auto flex items-center justify-center">
         Projects{" "}
         <span className="underline underline-offset-4 decoration-1 under font-light">
           {" "}
           Showcase
+        </span>
+      </h1> */}
+      <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-gray-900 mb-6 text-center">
+        Projects{" "}
+        <span className="relative">
+          <span className="underline underline-offset-8 decoration-4 decoration-blue-600 font-light">
+            Showcase
+          </span>
         </span>
       </h1>
       {projectsData.map((p, i) => (
