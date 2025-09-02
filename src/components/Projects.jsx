767b6c5b16fb84690ebb73e79676da89f5845169
ibsa-a1 <ibsaabera712@gmail.com>
2025-09-02 23:36:13 +0300

    Projects page update

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,13 +7,6 @@ export default function Projects() {
       id="Projects"
       className="px-6 md:px-16 lg:px-24 py-12 mt-10 md:mt-16 sm:mt-8"
     >
-      {/* <h1 className="text-2xl sm:text-4xl md:text-5xl font-bold md:mb-32 mb-16 mx-auto flex items-center justify-center">
-        Projects{" "}
-        <span className="underline underline-offset-4 decoration-1 under font-light">
-          {" "}
-          Showcase
-        </span>
-      </h1> */}
       <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-gray-900 mb-6 text-center">
         Projects{" "}
         <span className="relative">
